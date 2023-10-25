76d1227792edc5a554b7d9aa02544cf3502656b7
stephanelsmith <stephane.smith@titansensor.com>
2023-10-25 16:06:34 +0000

    gps0: sdkconfig

--- ports/esp32/boards/IB_GPS0/mpconfigboard.cmake
+++ ports/esp32/boards/IB_GPS0/mpconfigboard.cmake
@@ -1,1 +1,17 @@
-../IB_TAG0/mpconfigboard.cmake
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.usb
+    boards/sdkconfig.ble
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram_sx
+    boards/IB_GPS0/sdkconfig.board
+)
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
+endif()
+
+#set(MICROPY_CROSS_FLAGS "-march=xtensawin -X emit=native")
+
+
