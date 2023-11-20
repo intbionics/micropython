615f6f9b0934f4cad7d4149ba6154a7683800942
stephanelsmith <stephane.smith@titansensor.com>
2023-11-20 14:56:11 +0000

    build: specifying the upy path so we can work with multiple micropython dirs

--- ports/esp32/build.py
+++ ports/esp32/build.py
@@ -1,13 +1,15 @@
 
 import os,sys,re
 
+
 if len(sys.argv) == 1:
+    upy_path = os.path.abspath('../..')
     print('''
 BUILD.PY
-python build.py -p ~/upyesp32s --boards IB_GPS0
-python build.py -p ~/upyesp32s --boards IB_GPS0 -c -dev -m \\"hello world\\"
-python build.py -p ~/upyesp32s --boards IB_GPS0 -c -live -m \\"hello world\\"
-''')
+python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0
+python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0 -c -dev -m \\"hello world\\"
+python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0 -c -live -m \\"hello world\\"
+'''.format(upy_path, upy_path, upy_path))
     exit()
 
 if '-p' in sys.argv:
