0bbfe148c65ac8faa636e18623b2c6a805948d8c
stephanelsmith <stephane.smith@titansensor.com>
2024-08-05 19:58:44 +0000

    add IB_GAT0 board

--- ports/esp32/build.py
+++ ports/esp32/build.py
@@ -9,6 +9,8 @@ if len(sys.argv) == 1:
     print('''
 BUILD.PY
 python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0
+python build.py -p ~/upyesp32s --upy_path {} --boards IB_GAT0
+
 python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0 -c -dev -m \\"hello world\\"
 python build.py -p ~/upyesp32s --upy_path {} --boards IB_GPS0 -c -live -m \\"hello world\\"
 '''.format(upy_path, upy_path, upy_path))
